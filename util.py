36700d27eaf904cdf4e4a001b01287c3398f13e5
syuyuusyu <syuyuusyu@gmail.com>
2023-12-10 09:47:38 +0800

    food 101

--- util.py
+++ util.py
@@ -9,29 +9,33 @@ def accuracy_fn(y_true:torch.Tensor,y_pred:torch.Tensor) -> float:
     return (correct / len(y_true)) * 100
 
 def train_step(module:nn.Module,data_loader:DataLoader,loss_fn = nn.Module,optimizer = torch.optim.Optimizer,accuracy_fn = None, device:torch.device = 'cpu')->Tuple:
-    train_loss,train_acc = 0,0
+    train_loss,train_acc,epoch_loss,epoch_acc = 0,0,[],[]
     for X,y in data_loader:
         module.to(device)
         X,y = X.to(device), y.to(device)        
         y_pred = module(X)
         loss = loss_fn(y_pred,y)
+        print(loss)
         if hasattr(module,'l2_regularization'):
             reg = module.l2_regularization()
             loss += reg
         train_loss += loss
+        epoch_loss.append(loss)
         optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(module.parameters(), max_norm=1)
         optimizer.step()
         if accuracy_fn:
-            train_acc += accuracy_fn(y,y_pred.argmax(dim=1))
+            acc = accuracy_fn(y,y_pred.argmax(dim=1))
+            train_acc += acc
+            epoch_acc.append(acc)
     train_loss /= len(data_loader)
     train_acc /= len(data_loader)
     print(f'train_loss:{train_loss}, trian:acc:{train_acc}')
-    return train_loss,train_acc
+    return train_loss,train_acc,epoch_loss,epoch_acc
 
 def test_step(module:nn.Module,data_loader:DataLoader,loss_fn = nn.Module,accuracy_fn = None, device:torch.device = 'cpu')->Tuple:
-    test_loss, test_acc = 0, 0
+    test_loss, test_acc,epoch_loss,epoch_acc = 0, 0,[],[]
     with torch.inference_mode():
         module.to(device)
         module.eval()
@@ -41,13 +45,16 @@ def test_step(module:nn.Module,data_loader:DataLoader,loss_fn = nn.Module,accura
             y_pred = module(X)
             #y_pred = y_pred.argmax(dim=1)
             loss = loss_fn(y_pred,y)
+            epoch_loss.append(loss)
             test_loss += loss
             if accuracy_fn:
-                test_acc += accuracy_fn(y,y_pred.argmax(dim=1))
+                acc = accuracy_fn(y,y_pred.argmax(dim=1))
+                epoch_acc.append(acc)
+                test_acc += acc
         test_loss /= len(data_loader)
         test_acc /= len(data_loader)
     print(f'test loss {test_loss}, test acc:{test_acc}')
-    return test_loss,test_acc
+    return test_loss,test_acc,epoch_loss,epoch_acc
 
 import requests
 def download_file(url, save_path):
